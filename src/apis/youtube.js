c9b45a501b2ab2aa6d68ee79844d18bb1cff3863
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-04-04 11:23:59 +0200

    Add .prettierrc

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,11 +1,11 @@
-import config from "../../src/config";
-import axios from "axios";
+import config from '../../src/config';
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3",
+  baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
-    part: "snippet",
-    type: "video",
+    part: 'snippet',
+    type: 'video',
     maxResults: 5,
     key: config.KEY,
   },
